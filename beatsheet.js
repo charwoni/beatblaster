eaa5e3a9cd4258667fc4c0e42fe983b368c02808
charwoni <charlie.caplan@gmail.com>
2017-11-06 20:22:07 -0800

    Add files via upload

---
+++ beatsheet.js
@@ -0,0 +1,188 @@
+var currentBeat = 0;
+var descDown = true;
+var nextBeatReady = false;
+
+$(document).ready(function(){
+    $("#arrowBar").click(function(){
+
+        nextBeat();
+        
+    });
+});
+
+$(document).ready(function(){
+    $("#start").click(function(){
+        $("#start").hide();
+        startGame(); 
+    });
+});
+
+$(document).on("keydown", function(event){ 
+        if(event.shiftKey && (event.which == 13)){
+            nextBeat();
+        };
+    
+});
+
+$(document).ready(function(){
+   
+});
+
+//$(document).ready(function(){
+//    $("textarea").keypress(function(event) {
+//        if (event.keyCode == 13 && event.shiftKey) {
+//         nextBeat(); 
+//         }
+//});
+//   
+//});
+
+//TO DO:
+//
+//set up objects that contain beat sheets
+//set up functions that take objects and generate beatDivs with id="beatDiv_i"
+//set all beat divs at top: 100%
+//function on down arrow to move through the beat divs
+//set up functions to get and store data from text areas
+//set up timer function to rush through steps, maybe with moving time bar and background growing red as you near time limit or something
+//maybe a rule where if you dont write enough words then you lose everything
+
+
+//Function that creates the html elements for the various individual Beat Divs
+function createBeatDivs(){
+    
+    for(i=0; i < beatsheet.length; i++){ //runs through all the beats
+            
+        textHtml = "<div class=\"beatDiv\" id=\"beatDiv_" + (i+1) +"\"> \
+                        <p class=\"beatTitle\">" + (i+1) + ". " + beatsheet[i].Title + "</p> \
+                        <div class=\"descArrow\" id=\"descArrow_" + (i+1) + "\"></div> \
+                        <p class=\"beatDescription\"><span>" + beatsheet[i].Description + "</span></p> \
+                        <hr> \
+                        <textarea class=\"beatField\"> </textarea> \
+                        </div>";
+
+        $("#centeringDiv").append(textHtml);
+
+        var beatDiv = "#beatDiv_" + (i+1);
+
+        //sets the CSS for each div so they start located off screen
+          $(beatDiv).css({"position": "fixed", "top": "100%", "margin": "auto", "opacity": "0"});  
+    }; //end of for loop
+    
+    $("#beatDiv_1").animate({top: "12%", opacity: "1.0"}, 1000);
+    
+    $("#beatDiv_1").children("textarea").focus();   
+    
+
+    
+    
+};
+
+
+function startGame(){
+    
+  //bring Arrow Bar up
+//    $("#arrowBar").animate({top: "94%"},400)
+  
+    $("#overlay").animate({opacity: "0.9"}, {duration: 1000, queue: false})
+//    setTimeout(function(){
+//        $("#topBar").css({filter: "blur(1.6px)"});
+//    }, 800);
+  //Create Beat Sheet Div HTML elements  
+    createBeatDivs();
+    
+    currentBeat = 1;
+    
+    $(".descArrow").click(function(){
+        var currentArrowID = "#descArrow_" + currentBeat;
+        expandDescription();
+    });
+
+     $(".beatField").on("input",function(){
+        var currentBeatID = "#beatDiv_" + currentBeat;
+        if($(currentBeatID).children(".beatField").val().length > 15){
+            nextBeatReady = true;
+            $("#arrowBar").css({filter:"saturate(100%)",opacity:"0.6"});
+        }
+    });
+    
+}
+
+
+function nextBeat(){
+    
+    if(!nextBeatReady){return;};
+    
+    var currentBeatID = "#beatDiv_" + currentBeat;
+    var nextBeatID = "#beatDiv_" + (currentBeat + 1);
+    
+//    $(currentBeatID + " .beatField").css({"color": "#DDD"});
+    $(currentBeatID + " .beatField").attr("disabled",true);
+    
+    beatsheet[currentBeat-1].userInput = $(currentBeatID + " .beatField").val();
+    
+    $(nextBeatID).animate({top: "12%", opacity: "1.0"}, 1300);
+    
+    $(currentBeatID).animate({top: "-76%", opacity: "0"}, 1300);
+    
+    $(nextBeatID).children("textarea").focus();   
+    
+    descDown = false;
+    
+    currentBeat++;
+    
+    if(currentBeat > beatsheet.length){
+        endGame();
+    };    
+    
+    nextBeatReady = false;
+    
+    $("#arrowBar").css({filter:"saturate(0%)",opacity:"0.2"});
+
+}
+
+
+
+function expandDescription(){
+    
+    var currentBeatID = "#beatDiv_" + currentBeat;
+    
+    if(descDown){
+        $(currentBeatID).children(".beatDescription").slideUp(250);
+        descDown = false;
+        $(currentBeatID).children("textarea").animate({height:"79%"},300);
+    }
+    else {
+        $(currentBeatID).children(".beatDescription").slideDown(250);
+        descDown = true;
+        
+        setTimeout(function(){
+            var descHeight = $(currentBeatID).children(".beatDescription").css("height");
+            var textareaHeight = $(currentBeatID).children("textarea").css("height");
+            var newHeight = "calc(79% - " + descHeight + ")";            $(currentBeatID).children("textarea").css("height",newHeight);
+        },300);
+         
+    };
+    
+   
+//    alert(descDown);
+}; 
+
+
+
+function endGame(){
+    
+    $("#arrowBar").animate({top:"100%"},300);
+    
+    $("#centeringDiv").append("<div id=\"endDiv\"></div>");
+    
+    for(i=0; i < beatsheet.length; i++){ //runs through all the beats
+            
+        var textHtml = "<p class=\"endTitle\">" + (i+1) + ". " + beatsheet[i].Title + "</p> \
+                        <p class=\"userInput\">" + beatsheet[i].userInput + "</p>";
+
+        $("#endDiv").append(textHtml);
+    };
+    
+    setTimeout(function(){$("#endDiv").animate({opacity:"1"},300);},1300);
+};
---
+++ beatsheet_declaration.js
@@ -0,0 +1,236 @@
+var beatsheet = [];
+
+var beat1 = {
+    Title: "Opening Image",
+    Description: "A visual that represents the struggle & tone of the story. A snapshot of the main character’s problem, before the adventure begins.",
+    userInput: "",
+};
+
+beatsheet.push(beat1);
+
+var beat2 = {
+    Title: "Set-up",
+    Description: "Expand on the “before” snapshot. Present the main character’s world as it is, and what is missing in their life",
+    userInput: "",
+};
+
+beatsheet.push(beat2);
+
+var beat3 = {
+    Title: "Theme Stated",
+    Description: "What your story is about; the message, the truth. Usually, it is spoken to the main character or in their presence, but they don’t understand the truth…not until they have some personal experience and context to support it.",
+    userInput: "",
+};
+
+beatsheet.push(beat3);
+
+//var beat4 = {
+//    Title: "Catalyst",
+//    Description: "The moment where life as it is changes. It is the telegram, the act of catching your loved-one cheating, allowing a monster onboard the ship, meeting the true love of your life, etc. The “before” world is no more, change is underway",
+//    userInput: "",
+//};
+//
+//beatsheet.push(beat4);
+//
+//var beat5 = {
+//    Title: "Debate",
+//    Description: "But change is scary and for a moment, or a brief number of moments, the main character doubts the journey they must take. Can I face this challenge? Do I have what it takes? Should I go at all? It is the last chance for the hero to chicken out.",
+//    userInput: "",
+//};
+//
+//beatsheet.push(beat5);
+//
+//var beat6 = {
+//    Title: "Break into Two",
+//    Description: "The main character makes a choice and the journey begins. We leave the “Thesis” world and enter the upside-down, opposite world of Act Two.",
+//    userInput: "",
+//};
+//
+//beatsheet.push(beat6);
+//
+//var beat7 = {
+//    Title: "B Story",
+//    Description: "This is when there’s a discussion about the Theme – the nugget of truth. Usually, this discussion is between the main character and the love interest. So, the B Story is usually called the “love story”.",
+//    userInput: "",
+//};
+//
+//beatsheet.push(beat7);
+//
+//var beat8 = {
+//    Title: "Fun and Games",
+//    Description: "This is when Craig Thompson’s relationship with Raina blooms, when Indiana Jones tries to beat the Nazis to the Lost Ark, when the detective finds the most clues and dodges the most bullets. This is when the main character explores the new world and the audience is entertained by the premise they have been promised.",
+//    userInput: "",
+//};
+//
+//beatsheet.push(beat8);
+//
+//var beat9 = {
+//    Title: "Midpoint",
+//    Description: "Dependent upon the story, this moment is when everything is “great” or everything is “awful”. The main character either gets everything they think they want (“great”) or doesn’t get what they think they want at all (“awful”). But not everything we think we want is what we actually need in the end.",
+//    userInput: "",
+//};
+//
+//beatsheet.push(beat9);
+//
+//var beat10 = {
+//    Title: "Bad Guys Close In",
+//    Description: "Doubt, jealousy, fear, foes both physical and emotional regroup to defeat the main character’s goal, and the main character’s “great”/“awful” situation disintegrates.",
+//    userInput: "",
+//};
+//
+//beatsheet.push(beat10);
+//
+//var beat11 = {
+//    Title: "All Is Lost",
+//    Description: "The opposite moment from the Midpoint: “awful”/“great”. The moment that the main character realizes they’ve lost everything they gained, or everything they now have has no meaning. The initial goal now looks even more impossible than before. And here, something or someone dies. It can be physical or emotional, but the death of something old makes way for something new to be born.",
+//    userInput: "",
+//};
+//
+//beatsheet.push(beat11);
+//
+//var beat12 = {
+//    Title: "Dark Night of the Soul",
+//    Description: "The main character hits bottom, and wallows in hopelessness. The Why hast thou forsaken me, Lord? moment. Mourning the loss of what has “died” – the dream, the goal, the mentor character, the love of your life, etc. But, you must fall completely before you can pick yourself back up and try again.",
+//    userInput: "",
+//};
+//
+//beatsheet.push(beat12);
+//
+//var beat13 = {
+//    Title: "Break into Three",
+//    Description: "Thanks to a fresh idea, new inspiration, or last-minute Thematic advice from the B Story (usually the love interest), the main character chooses to try again.",
+//    userInput: "",
+//};
+//
+//beatsheet.push(beat13);
+//
+//var beat14 = {
+//    Title: "Finale",
+//    Description: "This time around, the main character incorporates the Theme – the nugget of truth that now makes sense to them – into their fight for the goal because they have experience from the A Story and context from the B Story. Act Three is about Synthesis!",
+//    userInput: "",
+//};
+//
+//beatsheet.push(beat14);
+//
+//var beat15 = {
+//    Title: "Final Image",
+//    Description: "opposite of Opening Image, proving, visually, that a change has occurred within the character.",
+//    userInput: "",
+//};
+//
+//beatsheet.push(beat15);
+
+
+
+//BeatSheet
+
+//var beatsheet = [];
+//
+//var beat1 = {
+//    Title: "Opening Image",
+//    Description: "Description of First Beat Description of First Beat Description of First Beat Description of First Beat Description of First BeatDescription of First BeatDescription of First BeatDescription of First Beat Description of First Beat",
+//};
+//
+//beatsheet.push(beat1);
+//
+//var beat2 = {
+//    Title: "Theme Stated",
+//    Description: "Description of Second Beat",
+//};
+//
+//beatsheet.push(beat2);
+//
+//var beat3 = {
+//    Title: "Set-up",
+//    Description: "Description of Third Beat",
+//};
+//
+//beatsheet.push(beat3);
+//
+//var beat4 = {
+//    Title: "Catalyst",
+//    Description: "Description of Fourth Beat",
+//};
+//
+//beatsheet.push(beat4);
+//
+//var beat5 = {
+//    Title: "Debate",
+//    Description: "Description of Fifth Beat",
+//};
+//
+//beatsheet.push(beat5);
+//
+//var beat6 = {
+//    Title: "Break into Two",
+//    Description: "Description of Fifth Beat",
+//};
+//
+//beatsheet.push(beat6);
+//
+//var beat7 = {
+//    Title: "B Story",
+//    Description: "Description of Fifth Beat",
+//};
+//
+//beatsheet.push(beat7);
+//
+//var beat8 = {
+//    Title: "Fun and Games",
+//    Description: "Description of Fifth Beat",
+//};
+//
+//beatsheet.push(beat8);
+//
+//var beat9 = {
+//    Title: "Midpoint",
+//    Description: "Description of Fifth Beat",
+//};
+//
+//beatsheet.push(beat9);
+//
+//var beat10 = {
+//    Title: "Bad Guys Close In",
+//    Description: "Description of Fifth Beat",
+//};
+//
+//beatsheet.push(beat10);
+//
+//var beat11 = {
+//    Title: "All Is Lost",
+//    Description: "Description of Fifth Beat",
+//};
+//
+//beatsheet.push(beat11);
+//
+//var beat12 = {
+//    Title: "Dark Night of the Soul",
+//    Description: "Description of Fifth Beat",
+//};
+//
+//beatsheet.push(beat12);
+//
+//var beat13 = {
+//    Title: "Break into Three",
+//    Description: "Description of Fifth Beat",
+//};
+//
+//beatsheet.push(beat13);
+//
+//var beat14 = {
+//    Title: "Finale",
+//    Description: "Description of Fifth Beat",
+//};
+//
+//beatsheet.push(beat14);
+//
+//var beat15 = {
+//    Title: "Final Image",
+//    Description: "Description of Fifth Beat",
+//};
+//
+//beatsheet.push(beat15);
+
+
+//BeatSheet
+
