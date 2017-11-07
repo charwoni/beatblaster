d1968df7ad70b9b9076ecd1951c76ee92ccf550f
charwoni <charlie.caplan@gmail.com>
2017-11-06 20:55:18 -0800

    Add files via upload

--- beatsheet_declaration.js
+++ beatsheet_declaration.js
@@ -24,101 +24,101 @@ var beat3 = {
 
 beatsheet.push(beat3);
 
-//var beat4 = {
-//    Title: "Catalyst",
-//    Description: "The moment where life as it is changes. It is the telegram, the act of catching your loved-one cheating, allowing a monster onboard the ship, meeting the true love of your life, etc. The “before” world is no more, change is underway",
-//    userInput: "",
-//};
-//
-//beatsheet.push(beat4);
-//
-//var beat5 = {
-//    Title: "Debate",
-//    Description: "But change is scary and for a moment, or a brief number of moments, the main character doubts the journey they must take. Can I face this challenge? Do I have what it takes? Should I go at all? It is the last chance for the hero to chicken out.",
-//    userInput: "",
-//};
-//
-//beatsheet.push(beat5);
-//
-//var beat6 = {
-//    Title: "Break into Two",
-//    Description: "The main character makes a choice and the journey begins. We leave the “Thesis” world and enter the upside-down, opposite world of Act Two.",
-//    userInput: "",
-//};
-//
-//beatsheet.push(beat6);
-//
-//var beat7 = {
-//    Title: "B Story",
-//    Description: "This is when there’s a discussion about the Theme – the nugget of truth. Usually, this discussion is between the main character and the love interest. So, the B Story is usually called the “love story”.",
-//    userInput: "",
-//};
-//
-//beatsheet.push(beat7);
-//
-//var beat8 = {
-//    Title: "Fun and Games",
-//    Description: "This is when Craig Thompson’s relationship with Raina blooms, when Indiana Jones tries to beat the Nazis to the Lost Ark, when the detective finds the most clues and dodges the most bullets. This is when the main character explores the new world and the audience is entertained by the premise they have been promised.",
-//    userInput: "",
-//};
-//
-//beatsheet.push(beat8);
-//
-//var beat9 = {
-//    Title: "Midpoint",
-//    Description: "Dependent upon the story, this moment is when everything is “great” or everything is “awful”. The main character either gets everything they think they want (“great”) or doesn’t get what they think they want at all (“awful”). But not everything we think we want is what we actually need in the end.",
-//    userInput: "",
-//};
-//
-//beatsheet.push(beat9);
-//
-//var beat10 = {
-//    Title: "Bad Guys Close In",
-//    Description: "Doubt, jealousy, fear, foes both physical and emotional regroup to defeat the main character’s goal, and the main character’s “great”/“awful” situation disintegrates.",
-//    userInput: "",
-//};
-//
-//beatsheet.push(beat10);
-//
-//var beat11 = {
-//    Title: "All Is Lost",
-//    Description: "The opposite moment from the Midpoint: “awful”/“great”. The moment that the main character realizes they’ve lost everything they gained, or everything they now have has no meaning. The initial goal now looks even more impossible than before. And here, something or someone dies. It can be physical or emotional, but the death of something old makes way for something new to be born.",
-//    userInput: "",
-//};
-//
-//beatsheet.push(beat11);
-//
-//var beat12 = {
-//    Title: "Dark Night of the Soul",
-//    Description: "The main character hits bottom, and wallows in hopelessness. The Why hast thou forsaken me, Lord? moment. Mourning the loss of what has “died” – the dream, the goal, the mentor character, the love of your life, etc. But, you must fall completely before you can pick yourself back up and try again.",
-//    userInput: "",
-//};
-//
-//beatsheet.push(beat12);
-//
-//var beat13 = {
-//    Title: "Break into Three",
-//    Description: "Thanks to a fresh idea, new inspiration, or last-minute Thematic advice from the B Story (usually the love interest), the main character chooses to try again.",
-//    userInput: "",
-//};
-//
-//beatsheet.push(beat13);
-//
-//var beat14 = {
-//    Title: "Finale",
-//    Description: "This time around, the main character incorporates the Theme – the nugget of truth that now makes sense to them – into their fight for the goal because they have experience from the A Story and context from the B Story. Act Three is about Synthesis!",
-//    userInput: "",
-//};
-//
-//beatsheet.push(beat14);
-//
-//var beat15 = {
-//    Title: "Final Image",
-//    Description: "opposite of Opening Image, proving, visually, that a change has occurred within the character.",
-//    userInput: "",
-//};
-//
-//beatsheet.push(beat15);
+var beat4 = {
+    Title: "Catalyst",
+    Description: "The moment where life as it is changes. It is the telegram, the act of catching your loved-one cheating, allowing a monster onboard the ship, meeting the true love of your life, etc. The “before” world is no more, change is underway",
+    userInput: "",
+};
+
+beatsheet.push(beat4);
+
+var beat5 = {
+    Title: "Debate",
+    Description: "But change is scary and for a moment, or a brief number of moments, the main character doubts the journey they must take. Can I face this challenge? Do I have what it takes? Should I go at all? It is the last chance for the hero to chicken out.",
+    userInput: "",
+};
+
+beatsheet.push(beat5);
+
+var beat6 = {
+    Title: "Break into Two",
+    Description: "The main character makes a choice and the journey begins. We leave the “Thesis” world and enter the upside-down, opposite world of Act Two.",
+    userInput: "",
+};
+
+beatsheet.push(beat6);
+
+var beat7 = {
+    Title: "B Story",
+    Description: "This is when there’s a discussion about the Theme – the nugget of truth. Usually, this discussion is between the main character and the love interest. So, the B Story is usually called the “love story”.",
+    userInput: "",
+};
+
+beatsheet.push(beat7);
+
+var beat8 = {
+    Title: "Fun and Games",
+    Description: "This is when Craig Thompson’s relationship with Raina blooms, when Indiana Jones tries to beat the Nazis to the Lost Ark, when the detective finds the most clues and dodges the most bullets. This is when the main character explores the new world and the audience is entertained by the premise they have been promised.",
+    userInput: "",
+};
+
+beatsheet.push(beat8);
+
+var beat9 = {
+    Title: "Midpoint",
+    Description: "Dependent upon the story, this moment is when everything is “great” or everything is “awful”. The main character either gets everything they think they want (“great”) or doesn’t get what they think they want at all (“awful”). But not everything we think we want is what we actually need in the end.",
+    userInput: "",
+};
+
+beatsheet.push(beat9);
+
+var beat10 = {
+    Title: "Bad Guys Close In",
+    Description: "Doubt, jealousy, fear, foes both physical and emotional regroup to defeat the main character’s goal, and the main character’s “great”/“awful” situation disintegrates.",
+    userInput: "",
+};
+
+beatsheet.push(beat10);
+
+var beat11 = {
+    Title: "All Is Lost",
+    Description: "The opposite moment from the Midpoint: “awful”/“great”. The moment that the main character realizes they’ve lost everything they gained, or everything they now have has no meaning. The initial goal now looks even more impossible than before. And here, something or someone dies. It can be physical or emotional, but the death of something old makes way for something new to be born.",
+    userInput: "",
+};
+
+beatsheet.push(beat11);
+
+var beat12 = {
+    Title: "Dark Night of the Soul",
+    Description: "The main character hits bottom, and wallows in hopelessness. The Why hast thou forsaken me, Lord? moment. Mourning the loss of what has “died” – the dream, the goal, the mentor character, the love of your life, etc. But, you must fall completely before you can pick yourself back up and try again.",
+    userInput: "",
+};
+
+beatsheet.push(beat12);
+
+var beat13 = {
+    Title: "Break into Three",
+    Description: "Thanks to a fresh idea, new inspiration, or last-minute Thematic advice from the B Story (usually the love interest), the main character chooses to try again.",
+    userInput: "",
+};
+
+beatsheet.push(beat13);
+
+var beat14 = {
+    Title: "Finale",
+    Description: "This time around, the main character incorporates the Theme – the nugget of truth that now makes sense to them – into their fight for the goal because they have experience from the A Story and context from the B Story. Act Three is about Synthesis!",
+    userInput: "",
+};
+
+beatsheet.push(beat14);
+
+var beat15 = {
+    Title: "Final Image",
+    Description: "opposite of Opening Image, proving, visually, that a change has occurred within the character.",
+    userInput: "",
+};
+
+beatsheet.push(beat15);
 
 
 
